c3ecfab59dd1e41afdad6e7f5877dc5face915e2
jesustt15 <114711868+jesustt15@users.noreply.github.com>
2025-03-06 14:18:12 -0400

    pruebas NOJALES ESTO

--- front/src/router/AppRouter.jsx
+++ front/src/router/AppRouter.jsx
@@ -1,26 +1,45 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Home, Login } from '../auth';
-import { RutaProtegida } from './RutaProtegida';
+// import { RutaProtegida } from './RutaProtegida';
 import { Estudiantes, NewEstudiante } from '../estudiantes';
 import { NewUsuario, Usuarios } from '../usuarios';
-import { ProtectedLayout } from './ProtectedLayout';
+// import { ProtectedLayout } from './ProtectedLayout';
+import { Sidebar } from '../components';
+import { useState } from 'react';
 
 export const AppRouter = () => {
+  
+    const [menuVisible, setMenuVisible] = useState(true); // Estado para controlar la visibilidad del Sidebar
+    
+  
+  
+  
   return (
-    <Routes>
-      <Route path="/auth/*" element={<Login />} />
-      
-      <Route element={<RutaProtegida />}>
-        <Route element={<ProtectedLayout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/estudiantes" element={<Estudiantes />} />
-          <Route path="/estudiantes/new" element={<NewEstudiante />} />
-          <Route path="/usuarios" element={<Usuarios />} />
-          <Route path="/usuarios/new" element={<NewUsuario />} />
-        </Route>
-      </Route>
-      
-      <Route path="*" element={<Navigate to="/auth/" />} />
-    </Routes>
+ 
+      <div className="protected-layout">
+      {menuVisible && <Sidebar />}
+      <div className={`main-content ${menuVisible ? '' : 'expanded'}`}>
+        <div className="header">
+          <button
+            className="menu-toggle-button"
+            onClick={() => setMenuVisible(!menuVisible)}
+            aria-label="Toggle Menu"
+          >
+            <i className={`pi ${menuVisible ? 'pi-bars' : 'pi pi-bars'}`}></i>
+          </button>
+          <h1>Bienvenida, Silvia</h1>
+        </div>
+        <div className="content-area">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/estudiantes" element={<Estudiantes />} />
+            <Route path="/estudiantes/new" element={<NewEstudiante />} />
+            <Route path="/usuarios" element={<Usuarios />} />
+            <Route path="/usuarios/new" element={<NewUsuario />} />
+          </Routes>
+        </div>
+      </div>
+    </div>
+          
   );
 };
